a1509649a5774acf7c21d3c227d870be431e015d
gicziunicorn <giczidani100@gmail.com>
2025-09-05 13:21:17 +0200

    Add files via upload

--- weboldal/main.js
+++ weboldal/main.js
@@ -1,7 +1,14 @@
 let osztályok = {
-    "9":["angol3","matek4","tesi2"],
+    "9":["angol;3","matek;4","tesi;2"],
     "10":["matek3","angol2","magyar4"]
 }
 
 let órarend9 = []
 let órarend10 = []
+
+let óraszám9 = 0
+for(var i in osztályok[9]){
+    console.log(parseInt(osztályok[9][i]))
+}
+
+let árlagóra = 0
